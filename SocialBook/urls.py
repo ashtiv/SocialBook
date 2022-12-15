8ba06f7fae20520b8986b6e5fbd8bb9a9447bc8f
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-15 15:03:40 +0530

    first url

--- SocialBook/urls.py
+++ SocialBook/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name=index),
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
