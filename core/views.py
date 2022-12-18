711bb638c350dcc999559dab100b6674b249f039
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-18 14:16:36 +0530

    settings done

--- core/views.py
+++ core/views.py
@@ -174,33 +174,33 @@ def index(request):
 #         return redirect('/')
 
 
-# @login_required(login_url='signin')
-# def settings(request):
-#     user_profile = Profile.objects.get(user=request.user)
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
 
-#     if request.method == 'POST':
+    if request.method == 'POST':
 
-#         if request.FILES.get('image') == None:
-#             image = user_profile.profileimg
-#             bio = request.POST['bio']
-#             location = request.POST['location']
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
 
-#             user_profile.profileimg = image
-#             user_profile.bio = bio
-#             user_profile.location = location
-#             user_profile.save()
-#         if request.FILES.get('image') != None:
-#             image = request.FILES.get('image')
-#             bio = request.POST['bio']
-#             location = request.POST['location']
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
 
-#             user_profile.profileimg = image
-#             user_profile.bio = bio
-#             user_profile.location = location
-#             user_profile.save()
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
 
-#         return redirect('settings')
-#     return render(request, 'setting.html', {'user_profile': user_profile})
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 
 def signup(request):
@@ -233,7 +233,7 @@ def signup(request):
                 new_profile = Profile.objects.create(
                     user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
